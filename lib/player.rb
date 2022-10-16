5e800317a156dd10695739be6ad9b4bd5940d552
fell-z <matheusfelipeagapito@hotmail.com>
2022-10-15 22:14:52 -0300

    Make the player register the plays he makes

--- lib/player.rb
+++ lib/player.rb
@@ -11,14 +11,13 @@ class Player
   def make_a_play(possible_positions)
     loop do
       print "Player #{@player_number}, select a column to place your piece.\n>> "
-      desired_column = gets.chomp.to_i
-      # rubocop:disable Style
-      if desired_column.between?(1, 7) && possible_positions[desired_column - 1]
-        break possible_positions[desired_column - 1]
+      desired_column = gets.chomp.to_i - 1
+      if desired_column.between?(1, 7) && possible_positions[desired_column]
+        @plays << possible_positions[desired_column]
+        break possible_positions[desired_column]
       else
         puts "Oops! You've entered a invalid input or a unavailable column.\n\n"
       end
-      # rubocop:enable Style
     end
   end
 end
